1202b085a3d6e352228f9d6e4a2935b0ecc9f86b
Zak Mandhro <zak.mandhro@gmail.com>
2019-04-29 10:35:39 +0500

    first commit

--- config/index.js
+++ config/index.js
@@ -1,26 +1,27 @@
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteTitleManifest: 'MinimalBlog',
-  siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
-  siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
-  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
-  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
-  siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
+  siteTitle: "Isomorphic Graph Architecture", // Navigation and Site Title
+  siteTitleAlt: "Resources for Isomorphic Graph Architecture", // Alternative Site title for SEO
+  siteTitleManifest: "Graph Technology",
+  siteUrl: "https://graphtech.io", // Domain of your site. No trailing slash!
+  siteLanguage: "en", // Language Tag on <html> element
+  siteHeadline: "Resources for Isomorphic Graph Architecture", // Headline for schema.org JSONLD
+  siteBanner: "/social/banner.jpg", // Your image for og:image tag. You can find it in the /static folder
+  favicon: "src/favicon.png", // Your image for favicons. You can find it in the /src folder
+  siteDescription:
+    "This page was created by Zak Mandhro to promote the idea of building next generation software using Graph concepts and core Graph technologies. ", // Your site description
+  author: "Zak Mandhro", // Author for schemaORGJSONLD
+  siteLogo: "/social/logo.png", // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@minimal', // Twitter Username - Optional
-  ogSiteName: 'minimal', // Facebook Site Name - Optional
-  ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-6',
+  userTwitter: "@minimal", // Twitter Username - Optional
+  ogSiteName: "GraphTech.io", // Facebook Site Name - Optional
+  ogLanguage: "en_US", // Facebook Language
+  googleAnalyticsID: "UA-47519312-6",
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
-  themeColor: '#3498DB',
-  backgroundColor: '#2b2e3c',
+  themeColor: "#3498DB",
+  backgroundColor: "#2b2e3c"
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import styled from 'styled-components'
+import React from "react"
+import PropTypes from "prop-types"
+import { Link, graphql } from "gatsby"
+import styled from "styled-components"
 
-import { Layout, Article, Wrapper, Button, SectionTitle } from '../components'
+import { Layout, Article, Wrapper, Button, SectionTitle } from "../components"
 
 const Content = styled.div`
   grid-column: 2;
@@ -43,28 +43,49 @@ const Hero = styled.div`
 
 const IndexPage = ({
   data: {
-    allMdx: { edges: postEdges },
-  },
+    allMdx: { edges: postEdges }
+  }
 }) => (
   <Layout>
     <Wrapper>
       <Hero>
-        <h1>Hi.</h1>
+        <h1>Isomorphic Graph Architecture</h1>
         <p>
-          I&apos;m John Doe, a Senior UX Developer with five years of industry experience, specializing in developing
-          React apps with the best UX users can get.
+          It's time to rethink our Database and API technologies. The future of
+          software is in graphs.
         </p>
-        <Link to="/contact">
+        <p>
+          By combining the power of Graph Databases with the versatility of
+          GraphQL, we will be able to build highly interconnected, intelligent
+          and capable software for the future.
+        </p>
+        <p>
+          This page was created by{" "}
+          <a href="https://linkedin.com/in/zakmandhro">Zak Mandhro</a> to
+          promote the idea of building next generation software using Graph
+          concepts and core Graph technologies.
+          <div style={{ paddingTop: 16, fontSize: 20 }}>
+            This site built using GraphQL - powered by{" "}
+            <a href="https://gatsbyjs.org">Gatsby</a> and{" "}
+            <a href="https://netlify.com">Netlify</a>.
+          </div>
+        </p>
+        <Link to="https://linkedin.com/in/zakmandhro">
           <Button big>
-            <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              width="1792"
+              height="1792"
+              viewBox="0 0 1792 1792"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
             </svg>
-            Contact
+            Get In Touch
           </Button>
         </Link>
       </Hero>
       <Content>
-        <SectionTitle>Latest stories</SectionTitle>
+        <SectionTitle>Graph Technology Resources</SectionTitle>
         {postEdges.map(post => (
           <Article
             title={post.node.frontmatter.title}
@@ -86,9 +107,9 @@ export default IndexPage
 IndexPage.propTypes = {
   data: PropTypes.shape({
     allMdx: PropTypes.shape({
-      edges: PropTypes.array.isRequired,
-    }),
-  }).isRequired,
+      edges: PropTypes.array.isRequired
+    })
+  }).isRequired
 }
 
 export const IndexQuery = graphql`
