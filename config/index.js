ba6775b27c82d13dc538ee4bb43a0158fcd8bc80
Zak Mandhro <zak.mandhro@gmail.com>
2019-05-01 01:27:28 +0500

    image added

--- config/index.js
+++ config/index.js
@@ -18,7 +18,7 @@ module.exports = {
   userTwitter: "@minimal", // Twitter Username - Optional
   ogSiteName: "GraphTech.io", // Facebook Site Name - Optional
   ogLanguage: "en_US", // Facebook Language
-  googleAnalyticsID: "UA-47519312-6",
+  googleAnalyticsID: "UA-139351656-1",
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
