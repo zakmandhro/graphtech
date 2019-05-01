cf67c3d1bcf5f62f9e5b4ed0a0171c4a13fc780b
Zak Mandhro <zak.mandhro@gmail.com>
2019-05-01 11:39:47 +0500

    remove self promo

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,28 +58,6 @@ const IndexPage = ({
           and the versatility of GraphQL to enable next generation software to
           unlock the rich information hidden in our interconnected data.
         </p>
-        <p>
-          This site was created by{" "}
-          <a href="https://linkedin.com/in/zakmandhro">Zak Mandhro</a> to
-          promote the idea of building next generation software using Isomorphic
-          Graph Architecture.
-          <div style={{ paddingTop: 16, fontSize: 16 }}>
-            Built using GraphQL with <a href="https://gatsbyjs.org">Gatsby</a>.
-          </div>
-        </p>
-        <a href="https://linkedin.com/in/zakmandhro">
-          <Button big>
-            <svg
-              width="1792"
-              height="1792"
-              viewBox="0 0 1792 1792"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
-            </svg>
-            Get In Touch
-          </Button>
-        </a>
       </Hero>
       <Content>
         <SectionTitle>Graph Technology Resources</SectionTitle>
